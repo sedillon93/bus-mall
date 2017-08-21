b26a84242a8c8ac477b9cfa31a19f2d64c0456fa
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-21 13:39:43 -0700

    create array of products to be created by constructor iteration

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use-strict';
 
-function Product(name, clicks, shown, description) {
+function Product(name, path, clicks, shown) {
   this.name = name;
+  this.path = path;
   this.clicks = 0;
   this.shown = 0;
-  this.description = description;
 }
+
+var products = [['bag', bag.jpg], ['banana', banana.jpg], ['bathroom', bathroom.jpg], ['boots', boots.jpg], ['breakfast', breakfast.jpg], ['bubblegum', bubblegum.jpg], ['chair', chair.jpg], ['cthulthu', cthulthu.jpg], ['dogDuck', dogDuck.jpg], ['dragon', dragon.jpg], ['pen', pen.jpg], ['petSweep', petSweep.jpg], ['scissors', scissors.jpg], ['shark', shark.jpg], ['sweep', sweep.png], ['tauntaun', tauntaun.jpg], ['unicorn', unicorn.jpg],['usb', usb.gif], ['watercan', watercan.jpg], ['wineGlass', wineGlass.jpg]];
